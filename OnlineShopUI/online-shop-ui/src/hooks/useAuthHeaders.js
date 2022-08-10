0fd7284f8fb107d18075af5fae73d48113b4800b
Oleksandr Bondarenko <oleksandr.bondarenko@blueprism.com>
2022-08-10 16:18:27 +0300

    @SHP-62: Migration

--- OnlineShopUI/online-shop-ui/src/hooks/useAuthHeaders.js
+++
@@ -1,7 +0,0 @@
-import useAuth from './useAuth';
-
-export default function useAuthHeaders() {
-  const { user } = useAuth();
-
-  return { headers: { Authorization: user ? `Bearer ${user.token}` : null } };
-}
